4ce27e15da68d9442c7c4e8b84416d93348495e3
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-02-09 11:11:27 +0200

    firebase integration
    Using the Firestore DB to access tennis places.

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
+export const db = getFirestore(app);
--- frontend/src/pages/Tennis.js
+++ frontend/src/pages/Tennis.js
@@ -1,5 +1,21 @@
+"use client";
+import { db } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
 function TennisPage() {
+  loader();
+
   return <h1>Tennis</h1>;
 }
 
 export default TennisPage;
+
+async function loader() {
+  const querySnapshot = await getDocs(collection(db, "tennis"));
+  const data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+
+  console.log(data);
+}

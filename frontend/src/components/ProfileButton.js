6072dccdf84d610e366d97735e6a61ccea895822
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-02-09 16:26:40 +0200

    new profile icon
    Added as an img.

--- frontend/src/components/ProfileButton.js
+++ frontend/src/components/ProfileButton.js
@@ -1,13 +1,12 @@
 import classes from "./ProfileButton.module.css";
 
 import ProfilePicIcon from "./ProfilePicIcon";
+import profileLogo from "../assets/profile.png";
 
 function ProfileButton() {
   return (
     <button className={classes.button}>
-      <span classname={classes.icon}>
-        <ProfilePicIcon />
-      </span>
+      <img className={classes.icon} src={profileLogo} alt="Profile Logo" />
     </button>
   );
 }

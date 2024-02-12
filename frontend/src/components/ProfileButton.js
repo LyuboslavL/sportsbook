e5dfb74cf55cb72626375379a1d0d02f18b54076
Lyuboslav Lozanov <lyuboslav.v.lozanov@gmail.com>
2024-02-12 11:42:19 +0200

    dropdown profile menu
    As part of the main navigation.

--- frontend/src/components/ProfileButton.js
+++ frontend/src/components/ProfileButton.js
@@ -4,9 +4,18 @@ import profileLogo from "../assets/profile.png";
 
 function ProfileButton() {
   return (
-    <button className={classes.button}>
-      <img className={classes.icon} src={profileLogo} alt="Profile Logo" />
-    </button>
+    <div className={classes.dropdown}>
+      <button className={classes.button}>
+        <img className={classes.icon} src={profileLogo} alt="Profile Logo" />
+      </button>
+      <div className={classes.item}>
+        <ul>
+          <li>Profile</li>
+          <li>Reservations</li>
+        </ul>
+        <button>Logout</button>
+      </div>
+    </div>
   );
 }
 
